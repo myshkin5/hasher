4ab5d3bab371b5c6860c20312be07151d1f00864
Dwayne Schultz <dwayne@schultz.net>
2018-07-21 14:34:29 -0600

    Add availability delay

--- persistence/hash_store.go
+++ persistence/hash_store.go
@@ -13,28 +13,47 @@ type HashFunc func(string) string
 var ErrHashNotAvailable = errors.New("store: the requested hash is not available")
 
 type HashStore struct {
+	delay    time.Duration
 	hashFunc HashFunc
 
 	count uint64
 
-	hashes []string
+	hashes []hash
+}
+
+type hash struct {
+	available time.Time
+	hash      string
 }
 
 func NewHashStore(delay time.Duration, hashFunc HashFunc) *HashStore {
 	return &HashStore{
+		delay:    delay,
 		hashFunc: hashFunc,
-		hashes:   make([]string, 0),
+
+		hashes: make([]hash, 0),
 	}
 }
 
 func (s *HashStore) AddPassword(password string) uint64 {
 	c := atomic.AddUint64(&s.count, 1)
 	logs.Logger.Infof("Adding password, request %d...", c)
-	s.hashes = append(s.hashes, s.hashFunc(password))
+	s.hashes = append(s.hashes, hash{available: time.Now().Add(s.delay), hash: s.hashFunc(password)})
 	return c
 }
 
 func (s *HashStore) GetHash(requestId uint64) (string, error) {
 	logs.Logger.Infof("Returning hash for request %d", requestId)
-	return s.hashes[requestId-1], nil
+
+	if len(s.hashes) < int(requestId) {
+		return "", ErrHashNotAvailable
+	}
+
+	hash := s.hashes[requestId-1]
+
+	if time.Now().Before(hash.available) {
+		return "", ErrHashNotAvailable
+	}
+
+	return hash.hash, nil
 }
--- persistence/hash_store_test.go
+++ persistence/hash_store_test.go
@@ -27,6 +27,27 @@ func TestHashStore(t *testing.T) {
 		})
 	})
 
+	t.Run("GetHash", func(t *testing.T) {
+		t.Run("unavailable hashes returns error", func(t *testing.T) {
+			// ASSEMBLE
+			store := persistence.NewHashStore(time.Millisecond, func(input string) string { return input })
+
+			store.AddPassword("pass1")
+			store.AddPassword("pass2")
+
+			// ACT
+			hash, err := store.GetHash(3)
+
+			// ASSERT
+			if err != persistence.ErrHashNotAvailable {
+				t.Error("Error not returned for non-existent hash")
+			}
+			if hash != "" {
+				t.Error("Non-empty hash returned for non-existent hash")
+			}
+		})
+	})
+
 	t.Run("AddPassword/GetHash", func(t *testing.T) {
 		t.Run("returns expected hash", func(t *testing.T) {
 			// ASSEMBLE
@@ -38,6 +59,44 @@ func TestHashStore(t *testing.T) {
 			hash, err := store.GetHash(requestId)
 
 			// ASSERT
+			if err != nil {
+				t.Error("Returned unexpected error")
+			}
+			if hash != "hash1" {
+				t.Error("Did not return expected hash")
+			}
+		})
+
+		t.Run("delays hash availability", func(t *testing.T) {
+			// ASSEMBLE
+			store := persistence.NewHashStore(500*time.Millisecond, func(input string) string { return "hash1" })
+
+			// ACT
+			// ASSERT
+			requestId := store.AddPassword("pass1")
+			start := time.Now()
+			var hashAvailableTooSoon bool
+			for {
+				if time.Now().After(start.Add(450 * time.Millisecond)) {
+					break
+				}
+
+				_, err := store.GetHash(requestId)
+				if err != persistence.ErrHashNotAvailable {
+					hashAvailableTooSoon = true
+					break
+				}
+
+				time.Sleep(10 * time.Millisecond)
+			}
+
+			if hashAvailableTooSoon {
+				t.Error("Hash available too soon")
+			}
+
+			time.Sleep(time.Now().Sub(start))
+
+			hash, err := store.GetHash(requestId)
 			if err != nil {
 				t.Error("Returned unexpected error")
 			}
